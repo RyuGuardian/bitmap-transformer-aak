d30aef07b559423308b800c113bff9bc1e7da78d
Austin King <aust.a.king@gmail.com>
2015-09-08 11:04:21 -0700

    Adjusted bmpTransformer function and test (test doesn't work)

--- test/bitmap_transformer_test.js
+++ test/bitmap_transformer_test.js
@@ -5,11 +5,10 @@ var bmpTr = require(__dirname + '/../lib/bitmap_transformer');
 var transform = require(__dirname + '/../lib/transform');
 
 describe('#bitmapTranformer', function() {
-		it("should returned undefined if successful", function() {
-			expect(bmpTr(
-				__dirname + '/../images/non-palette-bitmap.bmp',
-				__dirname + '/../images/npb-test.bmp', 
-				transform))
-			.to.eql(undefined);
+		var err = new Error();
+
+    //Couldn't get this test to work. Also couldn't figure out proper test for successful file read, because async.
+		it("should throw an error if the file doesn't exist", function() {
+			expect(bmpTr(__dirname + '/palette.bmp', __dirname + 'new.bmp', transform)).to.throw(err);
 		});
 });
